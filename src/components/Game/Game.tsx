a0f4ff6fba51479b9349d6843ff9fb32895ee567
Julia Pereira <juliapereira.odc@gmail.com>
2022-08-15 21:13:10 -0300

    commit de segubda-feira

--- src/components/Game/Game.tsx
+++ src/components/Game/Game.tsx
@@ -1,75 +1,153 @@
-import { useState } from 'react';
-import { Button, Container, Row } from 'react-bootstrap';
+import { useState } from "react";
+import { Button, Container, Row } from "react-bootstrap";
+import _ from "lodash";
 
-import AngularLogo from '../../assets/angular.svg';
-import BackboneLogo from '../../assets/backbone.svg';
-import EmberLogo from '../../assets/ember.svg';
-import ReactLogo from '../../assets/react.svg';
-import VueLogo from '../../assets/vue.svg';
+import AngularLogo from "../../assets/angular.svg";
+import BackboneLogo from "../../assets/backbone.svg";
+import EmberLogo from "../../assets/ember.svg";
+import ReactLogo from "../../assets/react.svg";
+import VueLogo from "../../assets/vue.svg";
 
-import { Card } from './components/Card'
-import { HiddenCard } from './components/HiddenCard';
+import { Card } from "./components/Card";
+import { HiddenCard } from "./components/HiddenCard";
+import { isTemplateExpression } from "typescript";
 
 type Structure = {
-    text: string;
-    image: string;
-    showing: boolean;
-    position: number[];
-}
+  text: string;
+  image: string;
+  showing: boolean;
+};
 
 const CardOptions: Structure[] = [
-    {
-        text: 'Angular',
-        image: AngularLogo,
-        showing: false,
-        position: [],
-    },
-    {
-        text: 'Backbone',
-        image: BackboneLogo,
-        showing: false,
-        position: [],
-    },
-    {
-        text: 'Ember',
-        image: EmberLogo,
-        showing: false,
-        position: [],
-    },
-    {
-        text: 'Vue',
-        image: VueLogo,
-        showing: false,
-        position: [],
-    },
+  {
+    text: "Angular",
+    image: AngularLogo,
+    showing: false,
+  },
+  {
+    text: "Backbone",
+    image: BackboneLogo,
+    showing: false,
+  },
+  {
+    text: "Ember",
+    image: EmberLogo,
+    showing: false,
+  },
+  {
+    text: "Vue",
+    image: VueLogo,
+    showing: false,
+  },
+  {
+    text: "React",
+    image: ReactLogo,
+    showing: false,
+  },
+  {
+    text: "Angular",
+    image: AngularLogo,
+    showing: false,
+  },
+  {
+    text: "Backbone",
+    image: BackboneLogo,
+    showing: false,
+  },
+  {
+    text: "Ember",
+    image: EmberLogo,
+    showing: false,
+  },
+  {
+    text: "Vue",
+    image: VueLogo,
+    showing: false,
+  },
+  {
+    text: "React",
+    image: ReactLogo,
+    showing: false,
+  },
 ];
 
 export const Game: React.FC = () => {
+  const [state, setState] = useState({
+    initialized: false,
+    gameStructure: [] as Structure[],
+    selections: [] as number[],
+  });
 
-    const [state, setState] = useState({
-        initialized: false,
-        gameStructure: [] as Structure[]
-    })
+  const initGame = () => {
+    setState({
+      initialized: true,
+      gameStructure: _.shuffle(CardOptions),
+      selections: [],
+    });
+  };
 
-    const initGame = () => {
-  
+  const onClickHiddenCard = (index: number) => {
+    let updatedGameStructure = [...state.gameStructure];
+    updatedGameStructure[index].showing = true;
 
+    if (state.selections.length === 0) {
+      setState({
+        ...state,
+        selections: [index],
+        gameStructure: updatedGameStructure,
+      });
+      return;
     }
 
-    if(!state.initialized){
-        return(
-            <Button onClick={initGame}>
-                Iniciar o Jogo
-            </Button>
-        )
+    setState({
+      ...state,
+      selections: [state.selections[0], index],
+      gameStructure: updatedGameStructure,
+    });
+
+    setTimeout(validateSelections, 1000);
+  };
+
+  const validateSelections = () => {
+    const [first, second] = state.selections;
+    const firstItem = state.gameStructure[first];
+    const secondItem = state.gameStructure[second];
+
+    if (firstItem.text !== secondItem.text) {
+      let updatedGameStructure = [...state.gameStructure];
+      updatedGameStructure[first].showing = false;
+      updatedGameStructure[second].showing = false;
+      setState({
+        ...state,
+        gameStructure: updatedGameStructure,
+      });
+      return;
     }
 
-    return (
-        <Container>
-            <Row>
-                <Card text="Angular" image={AngularLogo} />
-                <HiddenCard/>
-            </Row>
-        </Container>
-    );
-}
+    if (state.gameStructure.every((x) => x.showing)) {
+      alert("Você terminou o jogo");
+    }
+
+    setState({...state, selections: []})
+  };
+
+  if (!state.initialized) {
+    return <Button onClick={initGame}>Iniciar o Jogo</Button>;
+  }
+
+  console.log(state);
+
+  return (
+    <Container>
+      <Row>
+        {state.gameStructure.map((item, key) =>
+          item.showing ? (
+            <Card key={key} image={item.image} text={item.text} />
+          ) : (
+            <HiddenCard key={key} onClick={() => onClickHiddenCard(key)} />
+          )
+        )}
+      </Row>
+    </Container>
+  );
+};
--- src/components/Game/components/HiddenCard/HiddenCard.tsx
+++ src/components/Game/components/HiddenCard/HiddenCard.tsx
@@ -1,8 +1,12 @@
 import { Col, Card } from 'react-bootstrap';
 
-export const HiddenCard: React.FC = () => {
+type THiddenCardProps = {
+    onClick: () => void;
+}
+
+export const HiddenCard: React.FC<THiddenCardProps> = ({onClick}) => {
     return (
-        <Col lg={3} style={{cursor: 'pointer'}}>
+        <Col lg={3} style={{cursor: 'pointer', marginBottom: '20px'}} onClick={onClick}>
             <Card style={{height: '100%', justifyContent: 'center', display: 'flex,'}}>
                 <span style={{
                     fontSize: '80px',
